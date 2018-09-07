ad3d9aa8d8bbcae4e4403ba6c9bfdbc26a4e59b6
Oscar Uribe <oscardjuribe@gmail.com>
2018-09-07 18:20:19 -0500

    Board Class

--- board.py
+++ board.py
@@ -1,9 +1,9 @@
 import numpy as np
 import random
 from cell import Cell
-class Game:
+class Board:
 	"""
-		Game Class
+		Game Board
 	"""
 	def __init__(self,height,width,n_mines):
 		self.height=height
@@ -11,14 +11,7 @@ class Game:
 		self.n_mines=n_mines
 		self.fill_board()
 		self.random_mines()
-		self.count_mines()
-
-	def random_mines(self):	
-		mines=[random.randint(1,self.height*self.width) for x in range(0,self.n_mines) ]
-		for mine in mines:
-			i=(mine-1)/self.width
-			j=(mine-1)%self.width
-			self.board[i][j].set_has_mine(True)
+		self.count_neighbors()
 
 	def get_height(self):
 		return self.height
@@ -29,6 +22,13 @@ class Game:
 	def get_board(self):
 		return self.board
 
+	def random_mines(self):	
+		self.mines=[random.randint(1,self.height*self.width) for x in range(0,self.n_mines) ]
+		for mine in self.mines:
+			i=(mine-1)/self.width
+			j=(mine-1)%self.width
+			self.board[i][j].set_has_mine(True)
+
 	def fill_board(self):
 		self.board=[]
 		for x in range(0,self.height):
@@ -38,18 +38,49 @@ class Game:
 				row.append(cell)
 			self.board.append(row)
 
-	def count_mines(self):
+	def count_neighbors(self):
 		for x in range(0,self.height):
 			for y in range(0,self.width):			
-				self.board[x][y].values(self)
+				self.board[x][y].neighbor_mines(self)
+
+	def uncover_cell(self,i,j):
+		if(i>=0 and j>=0 and i<self.height and j<self.width):
+			if not self.board[i][j].get_state():
+				self.board[i][j].set_state(True)
+				self.print_board()
+		else:
+			print "Position not valid"
+
+	def mark_cell(self,i,j):
+		if(i>=0 and j>=0 and i<self.height and j<self.width):
+
+			if not self.board[i][j].get_state():
+
+				if not self.board[i][j].get_mark():
+					self.board[i][j].set_mark(True)
+					self.print_board()
+					print "The cell ("+str(i)+","+str(j)+") has been marked"
+				else:
+					self.board[i][j].set_mark(False)
+					self.print_board()
+					print "The cell ("+str(i)+","+str(j)+") has been unmarked"
+		else:
+			print "Position not valid"			
+
 
 	def print_board(self):
+		print ""
 		for x in range(0,self.height):
 			line=""
 			for y in range(0,self.width):
 				line+=str(self.board[x][y])+" "
 			print line
-a=Game(6,10,5)
+		print ""
+		print "--------------------------"	
+		print ""		
+			
+a=Board(6,10,2)
 a.print_board()
 
-
+a.mark_cell(2,3)
+a.mark_cell(2,4)
--- cell.py
+++ cell.py
@@ -4,20 +4,38 @@ class Cell:
 	each cell has 
 	"""
 	def __init__(self,pos_x,pos_y):
-		self.active=True
+		#initial state
+		self.state=False
+
 		#initial value
 		self.value="-"
-		#has mine
+
+		#has mine?
 		self.has_mine=False
+		#has mark?
+		self.mark=False
+
 		#location
 		self.pos_x=pos_x
 		self.pos_y=pos_y
 
-	def __str__(self):
-		if self.active:
-			return self.value
-		else:
-			return "."			
+	def get_value(self):
+		return self.value
+
+	def get_state(self):
+		return self.state
+
+	def get_mark(self):
+		return self.mark
+
+	def set_mark(self,mark):
+		self.mark=mark
+
+	def set_value(self,value):
+		self.value=value		
+		
+	def set_state(self,state):
+		self.state=state
 
 	def set_has_mine(self,has_mine):
 		self.has_mine=has_mine
@@ -26,18 +44,24 @@ class Cell:
 		else:
 			self.value="-"			
 
-	def get_value(self):
-		return self.value
+	def __str__(self):
+		if self.mark:
+			return "P"
+		elif self.state:
+			return self.value
+		else:
+			return "."	
 
-	def values(self,game):
+	def neighbor_mines(self,game):
 		if not self.has_mine:
-			neigbor_mines=0
+			neighbor_mines=0
 			for i in range(-1,2):
 				for j in range(-1,2):
 						if self.pos_x+i>=0 and self.pos_y+j>=0 and self.pos_x+i<game.get_height() and self.pos_y+j<game.get_width():
 							if game.get_board()[self.pos_x+i][self.pos_y+j].get_value()=="*":
 								if game.get_board()[self.pos_x+i][self.pos_y+j]!=self:
-									neigbor_mines+=1
-			if neigbor_mines!=0:
-				self.value=str(neigbor_mines)
+									neighbor_mines+=1
+			if neighbor_mines!=0:
+				self.value=str(neighbor_mines)
+				self.neighbor_mines=neighbor_mines
 
