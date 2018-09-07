012c0f9e4ea0ab816f6a3919893acecf8416dafa
Oscar Uribe <oscardjuribe@gmail.com>
2018-09-07 16:17:11 -0500

    Create Cell Class

---
+++ Board.py
@@ -0,0 +1,16 @@
+import numpy as np
+class board:
+
+	def __init__(self,height,width,n_mines):
+		self.height=height
+		self.width=width
+		self.n_mines=n_mines
+
+	def get_height(self):
+		return height
+	def get_width(self):
+
+	def random_mines(self):
+		
+		self.board=[[]]
+
