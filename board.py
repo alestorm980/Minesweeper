aa5d48e37b8edc26b4c81a19480c3f3348c11d5f
Oscar Uribe <oscardjuribe@gmail.com>
2018-09-07 20:55:35 -0500

    Ready to unit testing

--- board.py
+++ board.py
@@ -1,4 +1,3 @@
-import numpy as np
 import random
 from cell import Cell
 class Board:
@@ -7,16 +6,26 @@ class Board:
 		Contain a Cell array and some methods to change the board status
 	"""
 	def __init__(self,height,width,n_mines):
+		#game options
 		self.height=height
 		self.width=width
 		self.n_mines=n_mines
+
+		#initial functions
 		self.fill_board()
 		self.random_mines()
 		self.count_neighbors()
+
+		#array of marks
 		self.marks=[]
+		# does the game end?
+		self.end=False
 
 	def get_height(self):
 		return self.height
+
+	def get_end(self):
+		return self.end
 
 	def get_width(self):
 		return self.width
@@ -26,8 +35,7 @@ class Board:
 
 	def random_mines(self):	
 		# create n_mines random mines in a h*w board
-		#self.mines=[random.randint(1,self.height*self.width) for x in range(0,self.n_mines) ]
-		self.mines=[1,2]
+		self.mines=[random.randint(1,self.height*self.width) for x in range(0,self.n_mines) ]
 		for mine in self.mines:
 			#get the position from the actual mine
 			i=(mine-1)/self.width
@@ -55,13 +63,13 @@ class Board:
 	def init_uncover_cell(self,x,y):
 		#validate index
 		if(x>=0 and y>=0 and x<self.height and y<self.width):
-			if(self.board[x][y].get_value()=="*"):
+			if(self.is_a_loser(x,y)):
 				self.loser()
 			else:				
 				self.uncover_cell(x,y)
 				self.print_board()
 		else:
-			print "Position not valid"	
+			print "Position is not valid"	
 
 	def uncover_cell(self,x,y):
 		if(x>=0 and y>=0 and x<self.height and y<self.width):
@@ -77,6 +85,12 @@ class Board:
 							if x+i>=0 and y+j>=0 and x+i<self.height and y+j<self.width:
 								#uncover recursively for each neighbor
 								self.uncover_cell(x+i,y+j)			
+	def init_mark_cell(self,i,j):
+		if self.board[i][j].get_state():
+			print "Cell is not valid"
+		else:
+			self.mark_cell(i,j)			
+
 	# mark cell with P and change mark attribute
 	def mark_cell(self,i,j):
 		#validate index
@@ -87,6 +101,7 @@ class Board:
 					self.board[i][j].set_mark(True)
 					#add current mark to marks array
 					self.marks.append(self.board[i][j])
+					self.print_board()
 					print "The cell ("+str(i)+","+str(j)+") has been marked"
 					if self.is_a_winner():
 						self.congratulation()
@@ -95,10 +110,11 @@ class Board:
 					#set has mark as true
 					self.board[i][j].set_mark(False)
 					#delete mark
-					self.marks.remove(board[i][j])
+					self.marks.remove(self.board[i][j])
+					self.print_board()
 					print "The cell ("+str(i)+","+str(j)+") has been unmarked"
 		else:
-			print "Position not valid"
+			print "Position is not valid"
 
 	def is_a_winner(self):
 		if len(self.marks) == len(self.mines):
@@ -107,28 +123,49 @@ class Board:
 					return False
 			return True
 		else:
+			return False
+
+	def is_a_loser(self,x,y):
+		if self.board[x][y].get_value()=="*":
+			self.end=True
+			return True
+		else:
 			return False			
 
 	def congratulation(self):
+		self.end=True
 		print "=)"
 		print "CONGRATULATION YOU WIN!!!!"
+		print ""
 
 	def loser(self):
-		for mine in self.mine:
+		for mine in self.mines:
 			i=(mine-1)/self.width
 			j=(mine-1)%self.width
 			self.board[i][j].set_state(True)
 		self.print_board()			
+		print "=("
 		print "Sorry, you lost!!"
 		print "Try again"
+		print ""
+
 	#print board method
 	def print_board(self):
 		print ""
+		print "Board                                  Table of position"
+		print ""
+		i=0
+		j=0
 		for x in range(0,self.height):
 			line=""
+			line_aux=""
+			j=0
 			for y in range(0,self.width):
 				line+=str(self.board[x][y])+" "
-			print line
+				line_aux+="("+str(i)+","+str(j)+") "
+				j+=1
+			print line+"         "+line_aux
+			i+=1			
 		print ""
 		print "--------------------------"	
 		print ""
--- game.py
+++ game.py
@@ -12,21 +12,24 @@ class Game:
 		print "Please enter the height, width and number of mines that you want"
 
 	#parse input data for each game
-	def parse_game(self,line):
+	def play(self):
+		line=raw_input("Please enter the position and option\n")
 		split_line=line.split(' ')
+		if len(split_line)!=3:
+			return -1
 		try:
 			i=int(split_line[0])
 			j=int(split_line[1])
 			option=split_line[2]
 
 			if option =="U":
-				self.board.uncover_cell(i,j)
+				self.board.init_uncover_cell(i,j)
 			elif option =="M":
-				self.board.mark_cell(i,j)
+				self.board.init_mark_cell(i,j)
 			else:
 				print "Invalid Option"
 
-			self.board.print_board()
+			#self.board.print_board()
 		except ValueError:
 			print "Input error"
 			self.usage()
@@ -39,20 +42,38 @@ class Game:
 			self.height=int(split_line[0])
 			self.width=int(split_line[1])
 			self.n_mines=int(split_line[2])
+			if(self.n_mines>self.height*self.width):
+				print "The number of mines must be lower than the cells"
+				exit(0)
 		except:
-			print "Input Arguments"
+			print "Error!!!"
 			self.usage()
 			exit(0)
 
-	def start_game(self):
-		a=1
+	def print_g(self):
+		self.board.print_board()
+	
+	def is_end(self):
+		return self.board.end
+
+
 
 def main():
 	print "-------------------------------------------"
 	print "Welcome to the Minesweeper Game!!!"
 	print "-------------------------------------------"
 	print ""
-	g=Game()
+	salir=False
+	while not salir:
+		game=Game()
+		print "Creating new game..."
+		game.print_g()
+		while(not game.is_end()):
+			game.play()
+		option=raw_input("Play Again 1-Yes Other-No \n")
+		if option != "1":
+			salir=True
+
 
 
 
